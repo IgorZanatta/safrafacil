d25cb924664ee5eb6e1a95db6d8564b413a1fe8b
IgorZanatta <zanatta2014@outlook.com>
2024-07-31 12:02:15 -0400

    new

--- service/BaseService.tsx
+++ service/BaseService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL_API || 'http://localhost:8080',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL_API || 'https://safrafacilapi.onrender.com',
 });
 
 axiosInstance.interceptors.request.use(
