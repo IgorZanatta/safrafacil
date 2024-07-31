a7bc3a8c2e57d12de5f9573644b14c7c5045710c
IgorZanatta <zanatta2014@outlook.com>
2024-07-31 12:18:45 -0400

    aa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+const nextConfig = {
+  assetPrefix: isProd ? '/safrafacil/' : '',
+  basePath: isProd ? '/safrafacil' : '',
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
