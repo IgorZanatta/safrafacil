442ac487995f9c374e66992bf57e7dcc240a5475
IgorZanatta <zanatta2014@outlook.com>
2024-08-02 18:43:24 -0400

    asss

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: '/:path*',
-          destination: '/:path*' // Mapeia todas as rotas para as páginas corretas
-        }
-      ]
-    }
+    // Outras configurações conforme necessário
   }
   
