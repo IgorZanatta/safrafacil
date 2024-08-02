03dc711b78df007150fccb62fb45ce3fc21d11c4
IgorZanatta <zanatta2014@outlook.com>
2024-08-02 18:37:54 -0400

    as

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/:path*' // Mapeia todas as rotas para as páginas corretas
+        }
+      ]
+    }
+  }
+  
