d7b0c098a32518f8b8a049580dfd9a655ad9539b
IgorZanatta <zanatta2014@outlook.com>
2024-08-02 15:43:40 -0400

    nn

--- app/(main)/pages/home/page.tsx
+++ app/(main)/pages/home/page.tsx
@@ -234,7 +234,7 @@ const Fazenda = () => {
         if (id !== undefined) {
             localStorage.setItem('FAZENDA_ID', id.toString());
             console.log('FAZENDA_ID set in localStorage:', id); // Adicione este log
-            router.push(`/pages/setorList`);
+            router.push(`/pages/setor_List`);
         } else {
             console.error('ID da Fazenda é undefined');
         }
