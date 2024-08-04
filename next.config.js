2ebcde0d3a31d16407a9da530e80292feaa93340
IgorZanatta <zanatta2014@outlook.com>
2024-08-04 09:26:14 -0400

    sera deus?

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: true, // Adiciona barras no final das URLs
+  };
+  
+  module.exports = nextConfig;
+  
